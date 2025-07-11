953e0c6439a0d9a7705f4297e5841d78830e4173
Waterfountain10 <william.lafond@mail.mcgill.ca>
2025-07-11 19:27:43 -0400

    #5: added the local pre commit hook in .git

--- main.cpp
+++ main.cpp
@@ -1,16 +1,51 @@
 #include <iostream>
+#include <fstream>
+#include <format>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_timer.h>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+int main(int argc, char *argv[])
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "Error initializing SDL: " << SDL_GetError() << std::endl;
+        return 1;
     }
 
+    // default values
+    std::string rom_path;
+    std::ifstream rom_file;
+
+    try {
+        switch (argc) {
+            case 3: {
+                // CLI mode
+                rom_path = argv[1];
+                rom_file.open(rom_path, std::ios::in | std::ios::binary | std::ios::ate);  // pointing seeker at end
+                ipf = std::stoi(argv[2]);
+
+                if (!rom_file.is_open())
+                    throw std::runtime_error("<rom_path> file could not be opened.");
+                if (rom_file.tellg() > 4096)
+                    throw std::runtime_error("<rom_path> file size is too big for a GameBoy ROM");
+                if (rom_file.tellg() < 0)
+                    throw std::runtime_error("<rom_path> file size is negative");
+                break;
+            }
+            default: {
+                throw std::runtime_error("Incorrect number of arguments. Correct usage: ./chip-8-emulator <ROM_file> <ipf>");
+            }
+        }
+        std::cout << "-------------------------------------------------------" << std::endl;
+        std::cout << std::format("Running {}",argv[0]) << std::endl;
+        std::cout << std::format("---> ROM: {}", rom_path) << std::endl;
+        std::cout << "-------------------------------------------------------" << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 0;
+    }
+
+
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
