c619a8fabb01498ae0b7afd00ee28ffae899c9ea
Waterfountain10 <william.lafond@mail.mcgill.ca>
2025-07-19 18:19:50 -0400

    #2: implemented the display interface

--- platform-layer/display/display_interface.h
+++ platform-layer/display/display_interface.h
@@ -8,37 +8,14 @@
 class DisplayInterface {
 public:
     virtual ~DisplayInterface() = default; // uses compiler default deconstructor
-    // abstract methods
     virtual void clear() = 0;
     virtual void present_idle() = 0;
+    virtual void draw_pixel(int col, int row, bool draw_on);
+    int update_texture(int* gfx_ptr);
 protected:
-    int width;
-    int height;
+    int display_width;
+    int display_height;
 // base classes or interfaces should not have private methods/fields
 };
 
 #endif //DISPLAY_INTERFACE_H
-
-// public:
-// Gui(const std::string name, int width, int height, bool is_demo); // constructor
-// ~Gui();
-// void clear();
-// void present_idle();
-// bool input_rom_path(std::string rom_path);
-// void draw_pixel(int col, int row /* int scale */, bool on); // on, paint white, off is nothing
-//
-// int update_texture(uint8_t* gfx_ptr);
-//
-// private:
-// SDL_Window* win = nullptr;
-// SDL_Texture*  screen_texture;
-// std::unique_ptr<SDL_Rect> screen_rect;
-// SDL_Renderer* ren = nullptr;
-// SDL_Color* colors;
-//
-// uint8_t width;
-// uint8_t height;
-//
-// bool intro;
-//
-// SDL_Color* init_colors();
