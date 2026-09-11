ae8eb5065326dd172f3479c20bbff8d12e09e7d0
William Kiem Lafond <william.lafond@mail.mcgill.ca>
2026-09-11 17:25:45 -0400

    feat(interrupts): add ie if and servicing (#28)

--- src/gameboy/interrupts/interrupts.h
+++ src/gameboy/interrupts/interrupts.h
@@ -5,4 +5,39 @@
 #ifndef INTERRUPTS_H
 #define INTERRUPTS_H
 
+#include <cstdint>
+
+namespace GameBoy {
+
+class InterruptController {
+public:
+    enum Interrupt : uint8_t {
+        VBlank = 0,
+        Stat   = 1,
+        Timer  = 2,
+        Serial = 3,
+        Joypad = 4,
+    };
+
+    uint8_t read(uint16_t addr) const;
+    void    write(uint16_t addr, uint8_t value);
+
+    void request(Interrupt interrupt);
+    void set_ime(bool enabled);
+    bool ime() const;
+
+    bool     has_pending() const;
+    bool     can_service() const;
+    uint16_t service_next();
+
+private:
+    bool    ime_ = false;
+    uint8_t ie_  = 0x00;
+    uint8_t if_  = 0xE1;
+
+    uint8_t pending_() const;
+};
+
+} // namespace GameBoy
+
 #endif // INTERRUPTS_H
--- test/interrupts_test.cpp
+++ test/interrupts_test.cpp
@@ -1,3 +1,28 @@
-//
-// Created by William Lafond on 2025-07-08.
-//
+#include "gameboy/interrupts/interrupts.h"
+
+#include <gtest/gtest.h>
+
+TEST(Interrupts, ReadsAndWritesIeAndIfRegisters) {
+    GameBoy::InterruptController interrupts;
+
+    interrupts.write(0xFFFF, 0x1F);
+    interrupts.write(0xFF0F, 0x04);
+
+    EXPECT_EQ(interrupts.read(0xFFFF), 0x1F);
+    EXPECT_EQ(interrupts.read(0xFF0F), 0xE4);
+}
+
+TEST(Interrupts, RequestsAndServicesHighestPriorityPendingInterrupt) {
+    GameBoy::InterruptController interrupts;
+
+    interrupts.write(0xFFFF, 0x05);
+    interrupts.request(GameBoy::InterruptController::Timer);
+    interrupts.request(GameBoy::InterruptController::VBlank);
+    interrupts.set_ime(true);
+
+    EXPECT_TRUE(interrupts.has_pending());
+    EXPECT_TRUE(interrupts.can_service());
+    EXPECT_EQ(interrupts.service_next(), 0x0040);
+    EXPECT_FALSE(interrupts.ime());
+    EXPECT_EQ(interrupts.read(0xFF0F) & 0x1F, 0x04);
+}
